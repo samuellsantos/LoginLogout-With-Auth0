476a1cbf4d55c12155ccc7778de018eafbfffd97
Samuel Santos <samuka0102128@gmail.com>
2023-06-21 20:37:54 -0300

    add variables in the .env

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
-import { domain, clientId } from '../Keys.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Auth0Provider
-      domain={domain}
-      clientId={clientId}
+      domain={import.meta.env.VITE_REACT_APP_DOMAIN}
+      clientId={import.meta.env.VITE_REACT_APP_CLIENT_ID}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
